76ccca99c1074f39a88cc884289ba2171d8de2a9
fmsdwifull <fmsdwifull@sina.com>
2017-05-25 01:18:05 -0700

    发现css文件和js文件都不见了，没有样式的项目，悲催

--- application/config.php
+++ application/config.php
@@ -132,8 +132,8 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str'       => [
-        '__INDEX__'=>'http://192.168.199.191/tp5coweb/public/static/index',
-        '__APP__'=> 'http://192.168.199.191/tp5coweb'
+        '__INDEX__'=>'http://192.168.199.192/tp5coweb/public/static/index',
+        '__APP__'=> 'http://192.168.199.192/tp5coweb'
     ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
